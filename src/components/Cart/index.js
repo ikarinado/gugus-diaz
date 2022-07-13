1aac7e1510fbff9785741d533de71099ee4e5ffc
Karina <ikarina.do@gmail.com>
2022-07-12 23:48:48 -0300

    agreaga coleccion

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -8,13 +8,46 @@ import {
   Typography,
   List,
   Button,
+  Alert,
 } from "@mui/material";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { CartContext } from "../../CartContext";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
 
 function Cart() {
+  const [orderId, setOrderId] = useState(null);
   const { productos, removeProductos, vaciarCarrito } = useContext(CartContext);
   const cantP = productos.length;
+  const buyer = { name: "Agustin", phone: 1222, email: "agustin@gmail" };
+  const items = productos.map((item) => {
+    return {
+      id: item.producto.id,
+      titulo: item.producto.titulo,
+      precio: item.producto.precio,
+    };
+  });
+  let preciototal = 0;
+  productos.forEach((producto) => {
+    preciototal = preciototal + producto.producto.precio * producto.cantidad;
+  });
+
+  const setOrder = () => {
+    const body = {
+      buyer,
+      items,
+      date: new Date(),
+      total: preciototal,
+    };
+    const db = getFirestore();
+    const bodysCollection = collection(db, "orders");
+    addDoc(bodysCollection, body).then(({ id }) => {
+      setOrderId(id);
+    });
+  };
+  const irAinicio = () => {
+    vaciarCarrito();
+    window.location.href = "/";
+  };
 
   if (cantP === 0) {
     return (
@@ -25,11 +58,6 @@ function Cart() {
       </Card>
     );
   }
-
-  let preciototal = 0;
-  productos.forEach((producto) => {
-    preciototal = preciototal + producto.producto.precio * producto.cantidad;
-  });
 
   return (
     <div>
@@ -64,6 +92,23 @@ function Cart() {
       <Button onClick={() => vaciarCarrito()} variant='contained'>
         Limpiar carrito
       </Button>
+      {!orderId && (
+        <Button onClick={() => setOrder()} variant='contained'>
+          Terminar compra
+        </Button>
+      )}
+
+      {orderId && (
+        <div>
+          <Button onClick={() => irAinicio()} variant='contained'>
+            Seguir comprando
+          </Button>
+          <Alert severity='success'>
+            Tu compra se realizo correctamente.! El numero de tu compra es:{" "}
+            {orderId}
+          </Alert>
+        </div>
+      )}
     </div>
   );
 }
